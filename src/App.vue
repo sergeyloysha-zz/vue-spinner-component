83b6d672a892ce2e28c0afef6f1848171796c487
Sergey Loysha <sloysha@darwinapps.com>
2017-05-20 01:48:01 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,159 @@
+<template>
+
+    <div class="sl">
+        <div class="sl-container">
+            <div class="sl-content">
+                <h1 class="sl-title">VueJS Spinner Component</h1>
+
+                <div class="sl-preview">
+                    <div class="sl-preview__spinner">
+                        <spinner :status="spinnerStatus" :color="spinnerColor" :size="spinnerSize" :depth="spinnerDepth" :clockwise="spinnerClockwise" :duration="spinnerDuration"></spinner>
+                    </div>
+                </div>
+                
+                <div class="sl-controls">
+
+                    <div class="sl-controls__item">
+                        <label for="size">Spinner Status</label>
+                        <button v-model="spinnerStatus" @click="spinnerStatus = !spinnerStatus">{{ statusMessage }}</button>
+                    </div>
+
+                    <div class="sl-controls__item">
+                        <label for="size">Spinner Size</label>
+                        <input type="range" min="30" max="150" step="5" v-model="spinnerSize" id="size">
+                    </div>
+
+                    <div class="sl-controls__item">
+                        <label for="depth">Spinner Depth</label>
+                        <input type="range" min="1" max="10" step="1" v-model="spinnerDepth" id="depth">
+                    </div>
+
+                    <div class="sl-controls__item">
+                        <label for="color">Spinner Color</label>
+                        <input type="text"  v-model="spinnerColor" id="color">
+                    </div>
+
+                    <div class="sl-controls__item">
+                        <label for="clockwise">Spinner Clockwise rotation</label>
+                        <input type="checkbox" v-model="spinnerClockwise" id="clockwise">
+                    </div>
+
+                    <div class="sl-controls__item">
+                        <label for="duration">Spinner Duration</label>
+                        <input type="range" min="0.5" max="2" step="0.1" v-model="spinnerDuration" id="duration">
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    import Spinner from './components/Spinner.vue'
+
+    export default {
+        components: {
+            Spinner
+        },
+
+        data() {
+            return {
+                spinnerSize: 40,
+                spinnerStatus: true,
+                spinnerColor: '#627794',
+                spinnerDepth: 3,
+                spinnerClockwise: true,
+                spinnerDuration: 1.1,
+            }
+        },
+
+        computed: {
+            statusMessage() {
+                return this.spinnerStatus ? 'Active' : 'Inactive';
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss">
+
+    * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 62.5%;
+        background: #eff2f7;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    body {
+        margin: 0;
+        font: 400 1.4rem/1.4 'Helvetica Neue', Arial, sans-serif;
+        color: #3c4858;
+        min-width: 102rem;
+    }
+
+    a {
+        transition: color .25s;
+    }
+
+    .app {
+
+        &__inner {
+            padding: 8.9rem 4rem 2rem 4rem;
+        }
+
+    }
+
+    .sl {
+        &-container {
+            width: 100%;
+            max-width: 86rem;
+            margin: 0 auto;
+        }
+
+        &-content {
+            margin: 4rem 0;
+            background: #fff;
+            padding: 4rem;
+        }
+
+        &-title {
+            font-weight: 400;
+            text-align: center;
+            margin: 0 0 4rem 0;
+        }
+
+        &-preview {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 22rem;
+        }
+
+        &-controls {
+
+
+            &__item {
+                margin-bottom: 2rem;
+                display: flex;
+                align-items: center;
+
+                label {
+                    min-width: 10rem;
+                    margin-right: 2rem;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/Spinner.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="sl-spinner" v-show="status" :style="spinnerStyle"></div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+
+            status: {
+                type: Boolean,
+                default: true
+            },
+
+            clockwise: {
+                type: Boolean,
+                default: true
+            },
+
+            size: {
+                type: Number,
+                default: 30
+            },
+
+            depth: {
+                type: Number,
+                default: 3
+            },
+
+            duration: {
+                type: Number,
+                default: 1.1
+            },
+
+            color: {
+                type: String,
+                default: '#6589b6'
+            }
+
+        },
+
+        data() {
+            return {
+                clockwiseAnimations: ['rotateСlockwise', 'rotateCounterСlockwise'],
+                sizeUnits: 'px',
+                timeUnits: 's'
+            }
+        },
+
+        computed: {
+
+            spinnerClockwise() {
+                return this.clockwise ? this.clockwiseAnimations[0] : this.clockwiseAnimations[1];
+            },
+
+            spinnerSize() {
+                return this.size + this.sizeUnits;
+            },
+
+            spinnerDepth() {
+                return this.depth + this.sizeUnits;
+            },
+
+            spinnerDuration() {
+                return this.duration + this.timeUnits;
+            },
+
+            spinnerStyle() {
+                return {
+                    borderTopColor: this.hexToRGB(this.color, 0.15),
+                    borderRightColor: this.hexToRGB(this.color, 0.15),
+                    borderBottomColor: this.hexToRGB(this.color, 0.15),
+                    borderLeftColor: this.color,
+                    width: this.spinnerSize,
+                    height: this.spinnerSize,
+                    borderWidth: this.spinnerDepth,
+                    animationName: this.spinnerClockwise,
+                    animationDuration: this.spinnerDuration
+                }
+            }
+        },
+        methods: {
+            hexToRGB(hex, alpha) {
+                var r = parseInt(hex.slice(1, 3), 16),
+                    g = parseInt(hex.slice(3, 5), 16),
+                    b = parseInt(hex.slice(5, 7), 16);
+
+                if (alpha) {
+                    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+                } else {
+                    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+                }
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    .sl-spinner {
+        border-style: solid;
+        -webkit-transform: translateZ(0);
+        -ms-transform: translateZ(0);
+        transform: translateZ(0);
+
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+
+    }
+
+    @keyframes rotateСlockwise {
+
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+
+    }
+
+    @keyframes rotateCounterСlockwise {
+
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(-360deg);
+        }
+
+    }
+
+</style>
