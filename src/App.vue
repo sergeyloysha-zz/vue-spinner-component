68c8931349514686fecb44fb0a7bf544785b55b2
Sergey Loysha <sloysha@darwinapps.com>
2018-02-23 16:59:29 +0300

    updated gh-page

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,58 @@
 <template>
 
-    <div class="sl">
-        <div class="sl-container">
-            <div class="sl-content">
-                <h1 class="sl-title">Vue Spinner Component</h1>
-                <h3 class="sl-subtitle">vue-spinner-component</h3>
-                <div class="sl-socials">
-                    <a class="github-button" href="https://github.com/sergeyloysha/vue-spinner-component" data-icon="octicon-star" data-show-count="true" aria-label="Star sergeyloysha/vue-spinner-component on GitHub">Star</a>
-                </div>
+    <div class="app">
+        <div class="app__container">
+            <div class="app__inner">
+                <div class="app__info app__side">
+                    <div class="app__title"><h1>Vue<br/>Spinner<br/>Component</h1></div>
+                    <div class="app__likes">
+                        <a class="github-button" href="https://github.com/sergeyloysha/vue-spinner-component" data-icon="octicon-star" data-show-count="true" aria-label="Star sergeyloysha/vue-spinner-component on GitHub">Star</a>
+                    </div>
+                    <div class="app__sources">
+                        <a href="https://github.com/sergeyloysha/vue-spinner-component/archive/master.zip" class="app__btn">Download</a>
+                        <a href="https://github.com/sergeyloysha/vue-spinner-component" class="app__btn app__btn_transparent" target="_blank">Source on Github</a>
+                    </div>
 
-                <div class="sl-btns">
-                    <a href="https://github.com/sergeyloysha/vue-spinner-component" class="sl-btn" target="_blank">Source on Github</a>
-                    <a href="https://github.com/sergeyloysha/vue-spinner-component/archive/master.zip" class="sl-btn">Download</a>
+                    <div class="app__copy">
+                        &copy; 2018 Sergey Loysha / <a href="//loysha.com" target="_blank">loysha.com</a>
+                    </div>
                 </div>
-
-                <div class="sl-preview">
-                    <div class="sl-preview__spinner">
+                <div class="app__preview app__side">
+                    <div class="app__spinner">
                         <spinner :status="spinner.status" :color="spinner.color" :size="spinner.size" :depth="spinner.depth" :rotation="spinner.rotation" :speed="spinner.speed"></spinner>
                     </div>
+                    <div class="app__controls">
+                        <div class="sl-controls__item">
+                            <label for="size">Status</label>
+                            <button class="sl-controls__item-ctrl" v-model="spinner.status" @click="spinner.status = !spinner.status">{{ statusMessage }}</button>
+                        </div>
+
+                        <div class="sl-controls__item">
+                            <label for="size">Size</label>
+                            <input class="sl-controls__item-ctrl" type="range" min="30" max="150" step="5" v-model="spinner.size" id="size">
+                        </div>
+
+                        <div class="sl-controls__item">
+                            <label for="depth">Depth</label>
+                            <input class="sl-controls__item-ctrl" type="range" min="1" max="10" step="1" v-model="spinner.depth" id="depth">
+                        </div>
+
+                        <div class="sl-controls__item">
+                            <label for="color">Color</label>
+                            <input class="sl-controls__item-ctrl" type="text"  v-model="spinner.color" id="color" :style="{ color: spinner.color}">
+                        </div>
+
+                        <div class="sl-controls__item">
+                            <label for="clockwise">Rotation</label>
+                            <button class="sl-controls__item-ctrl" v-model="spinner.rotation" @click="spinner.rotation = !spinner.rotation">{{ rotationMessage }}</button>
+                        </div>
+
+                        <div class="sl-controls__item">
+                            <label for="speed">Speed</label>
+                            <input class="sl-controls__item-ctrl __rtl" type="range" min="0.5" max="2" step="0.01" v-model="spinner.speed" id="speed">
+                        </div>
+                    </div>
                 </div>
-                
-                <div class="sl-controls">
-
-                    <div class="sl-controls__item">
-                        <label for="size">Status</label>
-                        <button class="sl-controls__item-ctrl" v-model="spinner.status" @click="spinner.status = !spinner.status">{{ statusMessage }}</button>
-                    </div>
-
-                    <div class="sl-controls__item">
-                        <label for="size">Size</label>
-                        <input class="sl-controls__item-ctrl" type="range" min="30" max="150" step="5" v-model="spinner.size" id="size">
-                    </div>
-
-                    <div class="sl-controls__item">
-                        <label for="depth">Depth</label>
-                        <input class="sl-controls__item-ctrl" type="range" min="1" max="10" step="1" v-model="spinner.depth" id="depth">
-                    </div>
-
-                    <div class="sl-controls__item">
-                        <label for="color">Color</label>
-                        <input class="sl-controls__item-ctrl" type="text"  v-model="spinner.color" id="color">
-                    </div>
-
-                    <div class="sl-controls__item">
-                        <label for="clockwise">Rotation</label>
-                        <button class="sl-controls__item-ctrl" v-model="spinner.rotation" @click="spinner.rotation = !spinner.rotation">{{ rotationMessage }}</button>
-                    </div>
-
-                    <div class="sl-controls__item">
-                        <label for="speed">Speed</label>
-                        <input class="sl-controls__item-ctrl __rtl" type="range" min="0.5" max="2" step="0.1" v-model="spinner.speed" id="speed">
-                    </div>
-
-                </div>
-
             </div>
         </div>
     </div>
@@ -74,7 +73,7 @@
                 spinner: {
                     size: 50,
                     status: true,
-                    color: '#627794',
+                    color: '#4fc08d',
                     depth: 3,
                     rotation: true,
                     speed: 0.8,
@@ -97,6 +96,15 @@
 
 <style lang="scss">
 
+    $color-indigo: #5528ff;
+    $color-green: #4fc08d;
+    $color-black: #000000;
+    $color-default: #3c4858;
+    $color-white: #ffffff;
+    $color-bg: #eff2f7;
+
+    $sm: 768px;
+
     * {
         -webkit-box-sizing: border-box;
         -moz-box-sizing: border-box;
@@ -114,8 +122,7 @@
     body {
         margin: 0;
         font: 400 1.4rem/1.4 'Roboto', 'Helvetica Neue', Arial, sans-serif;
-        color: #3c4858;
-        min-width: 102rem;
+        color: $color-default;
     }
 
     a {
@@ -123,52 +130,123 @@
     }
 
     .app {
+        padding: 1rem 0;
 
-    }
+        @media screen and (min-width: $sm) {
+            padding: 5rem 0;
+        }
 
-    .sl {
-        &-container {
+        &__container {
             width: 100%;
-            max-width: 86rem;
+            max-width: 96rem;
             margin: 0 auto;
+            padding: 0 1rem;
         }
 
-        &-content {
-            margin: 4rem 0;
+        &__inner {
+            display: flex;
+            flex-direction: column;
+
+            @media screen and (min-width: $sm) {
+                flex-direction: row;
+                align-items: stretch;
+            }
+        }
+
+        &__side {
+            flex-shrink: 0;
+            padding: 5rem;
+
+            @media screen and (min-width: $sm) {
+                width: 50%;
+            }
+        }
+
+        &__preview {
+            border-radius: .4rem;
             background: #fff;
-            padding: 4rem;
         }
 
-        &-title {
-            font-weight: 300;
-            text-align: center;
-            margin: 0 0 1rem 0;
+        &__title {
+            h1 {
+                font-size: 5.2rem;
+                font-weight: 900;
+                line-height: 1.3;
+                margin: 0 0 3rem 0;
+                color: $color-green;
+            }
         }
 
-        &-subtitle {
-            font-weight: 300;
-            text-align: center;
-            margin: 0 0 2rem 0;
+        &__likes {
+            margin-bottom: 3rem;
         }
 
-        &-socials {
-            text-align: center;
-        }
-
-        &-preview {
+        &__spinner {
             display: flex;
             align-items: center;
             justify-content: center;
-            min-height: 22rem;
+            height: 15rem;
+            margin-bottom: 5rem;
         }
 
+        &__btn {
+            display: inline-block;
+            padding: 1rem 2rem;
+            background: $color-green;
+            color: #fff;
+            border-radius: .2rem;
+
+            margin: 0 1rem 0 0;
+
+            text-decoration: none;
+            outline: 0;
+            transition: opacity .15s linear;
+            border: 1px solid $color-green;
+
+            &:hover {
+                opacity: .8;
+            }
+
+            &_transparent {
+                background: transparent;
+                color: $color-default;
+                border-color: rgba($color-default, .1);
+            }
+        }
+
+        &__copy {
+            display: none;
+
+            @media screen and (min-width: $sm) {
+                display: block;
+                padding-top: 5rem;
+                color: rgba($color-black, .2);
+
+                a {
+                    color: $color-green;
+                    text-decoration: none;
+                }
+            }
+        }
+
+        &__controls {
+            margin: 0 -5rem -5rem -5rem;
+            padding: 5rem;
+            border-radius: 0 0 .4rem .4rem;
+        }
+    }
+
+    .sl {
         &-controls {
-            margin-top: 2rem;
 
             &__item {
                 margin-bottom: 2rem;
                 display: flex;
                 align-items: center;
+
+                &:last-child {
+                    margin-bottom: 0;
+                }
 
                 label {
                     font-weight: 300;
@@ -186,48 +264,80 @@
 
                 button {
                     border: 0;
-                    background: #eff2f7;
-                    padding: .6rem 1rem;
+                    background: $color-bg;
+                    color: $color-green;
+                    font-weight: 600;
+                    padding: 1rem;
                     border-radius: .4rem;
                     outline: none;
                     transition: background .25s;
                     cursor: pointer;
+                }
 
-                    &:hover {
-                        background: darken(#eff2f7, 5%);
-                    }
+                input {
+                    outline: 0;
                 }
 
                 input[type="range"].__rtl {
                     direction: rtl
                 }
 
-            }
-        }
+                input[type="text"] {
+                    font-weight: 600;
+                    border: 0;
+                    padding: 1rem;
+                    border-radius: .4rem;
+                    background: $color-bg;
+                }
 
-        &-btns {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-top: 3rem;
-            margin-bottom: 2rem;
-        }
+                input[type=range] {
+                    -webkit-appearance: none;
+                    width: 100%;
+                    background: $color-bg;
+                    max-width: 14.5rem;
+                    height: 1.5rem;
+                    border-radius: 1rem;
+                }
 
-        &-btn {
-            display: inline-block;
-            min-width: 20rem;
-            font-weight: 400;
-            color: #fff;
-            background: #627794;
-            padding: 1rem 2rem;
-            border-radius: .4rem;
-            transition: background .25s;
-            text-align: center;
-            margin: 0 1rem;
-            text-decoration: none;
+                input[type=range]::-webkit-slider-thumb {
+                    -webkit-appearance: none;
+                    width: 1.5rem;
+                    height: 1.5rem;
+                    background: $color-green;
+                    border-radius: 100%;
+                }
 
-            &:hover {
-                background: darken(#627794, 5%);
+                input[type=range]::-moz-range-thumb {
+                    -webkit-appearance: none;
+                    width: 1.5rem;
+                    height: 1.5rem;
+                    background: $color-green;
+                    border-radius: 100%;
+                    border: 0;
+                }
+
+                input[type=range]:focus {
+                    outline: none;
+                }
+
+                input[type=range]::-ms-track {
+                    width: 100%;
+                    cursor: pointer;
+
+                    border-color: transparent;
+                    color: transparent;
+                }
+
+                input[type=range]::-moz-range-track  {
+                    -webkit-appearance: none;
+                    width: 100%;
+                    background: $color-bg;
+                    max-width: 14.5rem;
+                    height: 1.5rem;
+                    border-radius: 1rem;
+                    outline: none;
+                }
+
             }
         }
     }
--- src/Spinner.vue
+++ src/Spinner.vue
@@ -1,144 +1,138 @@
 <template>
-    <div class="sl-spinner" v-show="status" :style="spinnerStyle"></div>
+  <div class="sl-spinner" v-show="status" :style="spinnerStyle"></div>
 </template>
 
 <script>
-    export default {
+  export default {
+    props: {
+      status: {
+        type: Boolean,
+        default: true
+      },
 
-        props: {
+      rotation: {
+        type: Boolean,
+        default: true
+      },
 
-            status: {
-                type: Boolean,
-                default: true
-            },
+      size: {
+        type: Number,
+        default: 30
+      },
 
-            rotation: {
-                type: Boolean,
-                default: true
-            },
+      depth: {
+        type: Number,
+        default: 3
+      },
 
-            size: {
-                type: Number,
-                default: 30
-            },
+      speed: {
+        type: Number,
+        default: 1.0
+      },
 
-            depth: {
-                type: Number,
-                default: 3
-            },
+      color: {
+        type: String,
+        default: '#6589b6'
+      }
+    },
 
-            speed: {
-                type: Number,
-                default: 1.0
-            },
+    data() {
+      return {
+        rotationAnimations: ['forward', 'backward'],
+        sizeUnits: 'px',
+        timeUnits: 's'
+      }
+    },
 
-            color: {
-                type: String,
-                default: '#6589b6'
-            }
+    computed: {
+      rotationDirection() {
+        return this.rotation ? this.rotationAnimations[0] : this.rotationAnimations[1];
+      },
 
-        },
+      spinnerSize() {
+        return this.size + this.sizeUnits;
+      },
 
-        data() {
-            return {
-                rotationAnimations: ['forward', 'backward'],
-                sizeUnits: 'px',
-                timeUnits: 's'
-            }
-        },
+      spinnerDepth() {
+        return this.depth + this.sizeUnits;
+      },
 
-        computed: {
+      spinnerSpeed() {
+        return this.speed + this.timeUnits;
+      },
 
-            rotationDirection() {
-                return this.rotation ? this.rotationAnimations[0] : this.rotationAnimations[1];
-            },
-
-            spinnerSize() {
-                return this.size + this.sizeUnits;
-            },
-
-            spinnerDepth() {
-                return this.depth + this.sizeUnits;
-            },
-
-            spinnerSpeed() {
-                return this.speed + this.timeUnits;
-            },
-
-            spinnerStyle() {
-                return {
-                    borderTopColor: this.hexToRGB(this.color, 0.15),
-                    borderRightColor: this.hexToRGB(this.color, 0.15),
-                    borderBottomColor: this.hexToRGB(this.color, 0.15),
-                    borderLeftColor: this.color,
-                    width: this.spinnerSize,
-                    height: this.spinnerSize,
-                    borderWidth: this.spinnerDepth,
-                    animationName: this.rotationDirection,
-                    animationDuration: this.spinnerSpeed
-                }
-            }
-        },
-        methods: {
-            hexToRGB(hex, alpha) {
-                var r = parseInt(hex.slice(1, 3), 16),
-                    g = parseInt(hex.slice(3, 5), 16),
-                    b = parseInt(hex.slice(5, 7), 16);
-
-                if (alpha) {
-                    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
-                } else {
-                    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-                }
-
-            }
+      spinnerStyle() {
+        return {
+          borderTopColor: this.hexToRGB(this.color, 0.15),
+          borderRightColor: this.hexToRGB(this.color, 0.15),
+          borderBottomColor: this.hexToRGB(this.color, 0.15),
+          borderLeftColor: this.color,
+          width: this.spinnerSize,
+          height: this.spinnerSize,
+          borderWidth: this.spinnerDepth,
+          animationName: this.rotationDirection,
+          animationDuration: this.spinnerSpeed
         }
+      }
+    },
+    methods: {
+      hexToRGB(hex, alpha) {
+        var r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+
+        if (alpha) {
+            return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+        } else {
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+      }
     }
+  }
 </script>
 
 <style lang="scss">
 
-    .sl-spinner {
-        border-style: solid;
-        -webkit-transform: translateZ(0);
-        -ms-transform: translateZ(0);
-        transform: translateZ(0);
+  .sl-spinner {
+    border-style: solid;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
 
-        animation-iteration-count: infinite;
-        animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
 
-        border-radius: 50%;
-        width: 30px;
-        height: 30px;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+  }
 
+  @keyframes forward {
+
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+
+  }
+
+  @keyframes backward {
+
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
     }
 
-    @keyframes forward {
-
-        0% {
-            -webkit-transform: rotate(0deg);
-            transform: rotate(0deg);
-        }
-
-        100% {
-            -webkit-transform: rotate(360deg);
-            transform: rotate(360deg);
-        }
-
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(-360deg);
     }
 
-    @keyframes backward {
-
-        0% {
-            -webkit-transform: rotate(0deg);
-            transform: rotate(0deg);
-        }
-
-        100% {
-            -webkit-transform: rotate(360deg);
-            transform: rotate(-360deg);
-        }
-
-    }
+  }
 
 </style>
